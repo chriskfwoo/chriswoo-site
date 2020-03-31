e490d7f9357b159094f02d9112c98e966579d1eb
chriskfwoo <chriskfwoo@gmail.com>
2020-03-31 19:45:48 -0400

    WIP: 2020 site

--- src/App.vue
+++ src/App.vue
@@ -1,117 +1,144 @@
 <template>
   <div id="app">
+    <!-- <div></div> -->
     <div class="container">
-      <div class="row">
-        <div class="one-half column">
+      <div class="cw-header">
+        <input checked type="checkbox" id="switch" name="theme" @change="toggleTheme()" />
+        <label for="switch">Toggle</label>
+        <h1 class="cw-title">
+          Chris Woo
+          <i class="em em-wave" aria-role="presentation" aria-label="wave"></i>
+        </h1>
+      </div>
+      <h2 class="cw-tagline">
+        software engineer & self-proclaimed ping pong pro
+        <i
+          class="em em-table_tennis_paddle_and_ball"
+          aria-role="presentation"
+          aria-label="TABLE TENNIS PADDLE AND BALL"
+        ></i>
+      </h2>
+      <div class="cw-socials">
+        <a v-for="link in links" :key="link.name" target="_blink" :href="link.href">
+          <ion-icon :name="link.icon"></ion-icon>
+        </a>
+      </div>
+      <p class="cw-info">
+        Sometimes, my friends call me
+        <i>Woooooooooooo or Woozzie</i>. Currently, I'm a fourth year software engineering student at
+        <span
+          class="extra"
+        >Concordia University</span> on my quest to get the iron ring.
+        <br />
+        <br />I also helped organize ConUHacks IV and V, the biggest hackathon in Montreal at Concordia University.
+        <br />
+        <br />I'm
+        <span class="extra">passionate</span> about travelling, sports, space, and coding. Some of my role models are Chris Hadfield, Elon Musk, Gary Vaynerchuk, Casey Neistat.
+      </p>
+      <div class="cw-experience">
+        <div class="cw-experience-item" v-for="job in jobs" :key="job.id">
           <div>
-            <h1 class="name">Welcome, I'm <span class="bold">Chris Woo</span>! 
-              <i class="em em-wave"></i>
-            </h1>
-            <h5>software engineer & self-proclaimed ping pong pro <i class="em em-table_tennis_paddle_and_ball"></i> </h5>
+            <img :src="job.img" height="35px" width="35px" />
           </div>
-          <div class="pics">
-            <img v-for="pic in selfies" :key="pic.id" :alt="pic.id" :src="pic.img" width="25%" height="25%" />
-          </div>
-          <div>
-          <p class="bio">
-            Sometimes, my friends call me “Woooooooooooo”. Currently, I'm a fourth year software engineering student at <span class="highlight-container"><span class="highlight">Concordia University</span></span> on my quest to get the iron ring. I'm passionate about travelling, sports, space, and coding. 
-            Some of my role models are Chris Hadfield, Elon Musk, Gary Vaynerchuk, Casey Neistat. <br /> <br />
-            Feel free to checkout out my <a class="bold" target="_blank" href="/files/chriswoo_resume.pdf">resume</a> 
-            or reach out to me at <a class="bold" target="_blank" href="mailto:chriskfwoo@gmail.com?subject=Hello!">chriskfwoo@gmail.com</a>.
-          </p>
-          <div>
-            <a v-for="link in links" :key="link.icon" target="_blink" :href="link.href">
-              <ion-icon :name="link.icon"></ion-icon>
-            </a>
-          </div>
-          </div>
-        </div>
-        <div class="one-half column">
-          <h3 class="highlight">Experience</h3>
-          <div class="experiences" v-for="exp in experiences" :key="exp.id">
-            <h6>{{exp.date}} <br /> 
-              <span class="bold">{{exp.position}} @ {{exp.company}}</span>
-            </h6>
-            <img :alt="exp.id" :src="exp.img" width="30%" height="30%" @click="redirect(exp.link)">
-          </div>
+          <div v-html="job.info" :class="job.id == 'drw2' && 'special'"></div>
         </div>
       </div>
+      <!-- <div class="cw-projects">
+        <div class="cw-projects-item" v-for="project in projects" :key="project.id">
+          <h4>
+            {{project.title}}
+            <i
+              class="em em-first_place_medal"
+              aria-role="presentation"
+              aria-label="FIRST PLACE MEDAL"
+            ></i>
+            {{project.subtitle}}
+          </h4>
+          <p>{{project.info}}</p>
+        </div>
+      </div> -->
     </div>
-  <footer>
-    <a href="/2019">2019</a>
-    <a href="/2018">2018</a>
-    <a href="/2017">2017</a>
-    <a href="/2016">2016</a>
-  </footer>
+    <!-- <div></div> -->
   </div>
 </template>
 
 <script>
-
 const linkedin = "https://www.linkedin.com/in/chriskfwoo";
 const github = "https://github.com/chriskfwoo";
 const email = "chriskfwoo@gmail.com";
 
 export default {
   name: "app",
+  created: function() {
+    document.documentElement.setAttribute("data-theme", "dark");
+  },
   data: function() {
     return {
+      theme: "dark",
       links: [
         {
           href: linkedin,
-          icon: "logo-linkedin"
-        },
-        {
-          href: `mailto:${email}?subject=Hello!`,
-          icon: "mail"
+          icon: "logo-linkedin",
+          name: "linkedin"
         },
         {
           href: github,
-          icon: "logo-github"
+          icon: "logo-github",
+          name: "github"
         },
         {
-          href: "/files/chriswoo_resume.pdf",
-          icon: "document"
+          href: "/files/chriswoo_resume2020.pdf",
+          icon: "document-text",
+          name: "resume"
+        },
+        {
+          href: `mailto:${email}?subject=Hello!`,
+          icon: "mail",
+          name: "email"
         }
       ],
-      experiences: [
+      jobs: [
+        {
+          id: "storetasker",
+          img: require("./assets/storetasker.png"),
+          info:
+            'I was excited to get my first internship at <span class="cw-experience-tag">Storetasker</span> (summer 2017). I was working on a web-app and gained a lot of UI skills.'
+        },
+        {
+          id: "drw1",
+          img: require("./assets/drw-black.png"),
+          info:
+            'I did my second internship at <span class="cw-experience-tag">DRW</span> (Jan 2018 - Dec 2018), I was there for a year while balancing school. I was part of the proprietary trading firm on the event-driven trading team. I was slowly becoming a full-stack engineer and learned a bit of the stock market along the way.'
+        },
         {
           id: "plotly",
-          date: "May 2019 - Present",
-          position: "Software Engineer Intern",
-          company: "Plotly",
           img: require("./assets/plotly.png"),
-          link: "http://plot.ly"
+          info:
+            'I pivoted and did my third internship at <span class="cw-experience-tag">Plotly</span> (summer 2019). It was awesome learning more about the open-source community and data visualization.'
         },
         {
-          id: "vigilant",
-          date: "Jan 2018 - Dec 2018",
-          position: "Software Developer Intern",
-          company: "DRW",
-          img: require("./assets/drw.png"),
-          link: "http://drw.com"
-        },
-        {
-          id: "carson",
-          date: "May 2017 - Aug 2017",
-          position: "Software Developer Intern",
-          company: "Carson eCommerce",
-          img: require("./assets/carson.png"),
-          link: "https://storetasker.com"
+          id: "drw2",
+          img: require("./assets/drw-black.png"),
+          info:
+            'In fall 2019, I pivoted back to <span class="cw-experience-tag">DRW</span> where I\'m currently a full-time Software Engineer under the event-driven trading team.'
         }
       ],
-      selfies: [
+      projects: [
         {
-          id: "me",
-          img: require("./assets/me.jpg")
+          id: "bonjour",
+          title: "Bonjour-Hi",
+          subtitle:
+            "1st place Facebook’s Mission @ Yale University - YHacks 2018",
+          info:
+            "A chatroom with bunch of people speaking in their own language but they’ll all hear the conversation in their own language."
         },
         {
-          id: "snowboard",
-          img: require("./assets/snowboard.jpg")
-        },
-        {
-          id: "waterfall",
-          img: require("./assets/waterfall.jpg")
+          id: "gifs",
+          title: "GIFS Agaisnt Humanity",
+          subtitle:
+            "1st place GIPHY API @ Boston University - BostonHacks 2018",
+          info:
+            "A multi-player web game that works like cards against humanity but with a set of random GIFS."
         }
       ]
     };
@@ -119,118 +146,223 @@ export default {
   methods: {
     redirect: function(link) {
       window.open(link, "_blank");
+    },
+    toggleTheme: function() {
+      if (this.theme == "dark") {
+        this.theme = "light";
+      } else {
+        this.theme = "dark";
+      }
+    }
+  },
+  watch: {
+    // whenever question changes, this function will run
+    theme: function(value) {
+      document.documentElement.classList.add("theme-transition");
+      document.documentElement.setAttribute("data-theme", value);
+      window.setTimeout(function() {
+        document.documentElement.classList.remove("theme-transition");
+      }, 1000);
     }
   }
-  
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+// THEMES
+html {
+  --primaryBackground: #ffffff;
+  --primaryTextColor: #000000;
+  --secondaryTextColor: #000000;
+  --tertiaryTextColor: #000000;
+  --fadedTextColor: #000000;
+}
+
+// dark theme (default)
+html[data-theme="dark"] {
+  --primaryBackground: #121212;
+  --secondaryBackground: #000000;
+  --primaryTextColor: #ffffff;
+  --secondaryTextColor: #03dac6;
+  --tertiaryTextColor: #9146ff;
+  --fadedTextColor: #c2c2c2;
+}
+
+
+html.theme-transition,
+html.theme-transition *,
+html.theme-transition *:before,
+html.theme-transition *:after {
+  transition: all 3s !important;
+  transition-delay: 0 !important;
+}
+
 body {
-  background-color: #ebf5fa;
+  background-color: var(--primaryBackground);
+  color: var(--primaryTextColor);
+  font-family: "Open Sans", sans-serif;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #212b36;
-  margin: 60px 0px;
+  // display: grid;
+  // grid-template-columns: repeat(3, 1fr);
+  padding-bottom: 50px;
 }
-.highlight {
-  text-decoration: none;
-  background-image: linear-gradient(#ffea8a, #ffea8a);
-  background-size: auto 15px;
-  background-repeat: repeat no-repeat;
-  background-position: bottom center;
-  padding-bottom: 0px;
-}
-ion-icon {
+h1 {
+  color: var(--primaryTextColor);
   font-size: 3.5rem;
-  padding-right: 15px;
-  &:hover {
-    opacity: 0.5;
-  }
 }
-.experiences {
-  padding-bottom: 25px;
+h2 {
+  color: var(--secondaryTextColor);
+  font-size: 1.8rem;
 }
-.name {
-  font-size: 4.5rem;
-}
-.em-wave {
-  height: 1em;
-  width: 1em;
-}
-.em-table_tennis_paddle_and_ball {
-  height: 1em;
-  width: 0.8em;
-}
-.container {
-  max-width: 100%;
-}
-.bottom,
-.top {
-  display: flex;
-  justify-content: space-between;
-}
-.bold {
+.extra {
+  color: var(--tertiaryTextColor);
   font-weight: bold;
 }
-.pics {
-  margin: 20px 0px;
-  img {
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-    @media (min-width: 1281px) {
-      margin-right: 35px;
+p {
+  font-size: 1.3rem;
+}
+.em-wave {
+  font-size: 2rem;
+}
+.cw-header {
+  padding-top: 50px;
+}
+.cw-title {
+  margin-bottom: 0px;
+}
+.cw-tagline {
+  margin: 0;
+}
+.cw-info {
+  padding: 30px 0px;
+}
+.cw-container {
+
+}
+// SOCIAL SECTION
+.cw-socials {
+  a {
+    padding-right: 10px;
+    color: var(--primaryTextColor);
+    text-decoration: none;
+  }
+  ion-icon {
+    font-size: 1.8em;
+    color: var(--primaryTextColor);
+  }
+}
+
+// EXPERIENCE SECTION
+.cw-experience {
+  background: #17141d;
+  padding: 20px;
+  box-shadow: 0 4px 80px #070509;
+  border-radius: 16px;
+  color: #c2c2c2;
+  &-item {
+    display: flex;
+    font-size: 1rem;
+    img {
+      margin-right: 15px;
+      border-radius: 0.5rem;
+    }
+    margin-bottom: 20px;
+    .special {
+      color: #fff;
+      font-weight: bold;
+    }
+    &:last-child {
+      margin-bottom: 0px;
+    }
+  }
+  &-tag {
+    font-weight: bold;
+    color: #018786;
+  }
+}
+
+// PROJECT SECTION
+.cw-projects {
+  padding-top: 50px;
+  color: var(--fadedTextColor);
+  &-item {
+    h4 {
+      margin-bottom: 0;
+    }
+    p {
+      font-size: 1rem;
+      margin-top: 0;
+    }
+    i {
+      margin-left: 15px;
     }
   }
 }
-.highlight-container,
-.highlight {
+
+// TOGGLE SWITCH
+input[type="checkbox"] {
+  height: 0;
+  width: 0;
+  visibility: hidden;
+}
+
+label {
+  cursor: pointer;
+  text-indent: -9999px;
+  width: 52px;
+  height: 27px;
+  background: grey;
+  float: right;
+  border-radius: 100px;
   position: relative;
 }
-.highlight-container {
-  display: inline-block;
-  &:before,
-  &:after {
-    content: " ";
-    display: block;
-    height: 90%;
-    width: 100%;
-    margin-left: -2px;
-    margin-right: -2px;
-    position: absolute;
-  }
-  &:before {
-    background: rgba(234, 221, 6, 0.9);
-    transform: rotate(0deg);
-    top: 2px;
-    left: 1px;
-  }
-  &:after {
-    background: rgba(234, 210, 6, 0.6);
-    top: 1px;
-    right: -1px;
-  }
-  .highlight {
-    color: #333;
-    z-index: 4;
+
+label:after {
+  content: "";
+  position: absolute;
+  top: 3px;
+  left: 3px;
+  width: 20px;
+  height: 20px;
+  background: #fff;
+  border-radius: 90px;
+  transition: 0.3s;
+}
+
+input:checked + label {
+  background: #0077ff;
+}
+
+input:checked + label:after {
+  left: calc(100% - 5px);
+  transform: translateX(-100%);
+}
+
+label:active:after {
+  width: 45px;
+}
+
+.container {
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+@media (min-width: 768px) {
+  .container {
+    width: 750px;
   }
 }
-a {
-  color: #202e78;
-  cursor: pointer;
-  &:hover {
-    color: #5c6ac4;
+@media (min-width: 992px) {
+  .container {
+    width: 970px;
   }
 }
-footer {
-  position: fixed;
-  bottom: 0;
-  a {
-    font-size: 18px;
-    text-decoration: none;
-    margin: 10px;
+@media (min-width: 1200px) {
+  .container {
+    width: 800px;
   }
 }
 </style>
